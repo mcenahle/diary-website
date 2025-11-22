c58307a0a78a88924232a8b6950d521f2e5cd2a8
mcenahle <admin@mail.mcenahle.cn>
2025-11-22 14:46:43 +0800

    更新左上角图片为 favicon.ico

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
         title: '在线日记本',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/favicon.ico',
         },
         items: [
           /* {
